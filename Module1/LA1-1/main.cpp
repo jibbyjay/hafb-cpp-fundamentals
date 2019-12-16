29678679738d5ea9582a6eeba4c0d34f36e8f49b
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:52:23 -0700

    Updated Changes

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream> 
+using namespace std;
 
 int main() 
 {
-   std::cout <<"Lucky " << std::endl; /* code */
+    int age;    //int: signed integers
+    //cout: for system output
+   std::cout <<"Lucky " << std::endl;
+   cout << "This is using namespace." << endl;
+   cout << "Wildcats"<<" "<<"WSU "<<"Odgen"<<endl;
+   // cin: for system/user input
+cout << "What is your age?" <<endl;
+   cin >> age;
+   // now return the input
+   cout << "I see you are "<< age << "!" <<endl;
     return 0;
 }
