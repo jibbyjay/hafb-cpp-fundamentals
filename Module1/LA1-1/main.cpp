5f5fc271b5f877a871db074a9b28dcf6b444bb7a
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 10:16:34 -0700

    more changes

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() 
 {
     int age;    //int: signed integers
+    int weight;
     //cout: for system output
    std::cout <<"Lucky " << std::endl;
    cout << "This is using namespace." << endl;
@@ -13,5 +14,12 @@ cout << "What is your age?" <<endl;
    cin >> age;
    // now return the input
    cout << "I see you are "<< age << "!" <<endl;
+   cout << "How much do you weigh?" <<endl;
+   cin >> weight;
+   cout << weight <<" is pretty heavy for someone who is "<<age<<endl;
+   // sizeof(): get the size of the variable
+   cout << "Integers are: "<< sizeof(int) << " bytes." << endl;
+   cout << "age + weight is " << age+weight<<endl;
+
     return 0;
 }
