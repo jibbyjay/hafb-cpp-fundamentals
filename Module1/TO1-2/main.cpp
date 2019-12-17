18b672783c1e8bad5602eabc600f2fc872365d4e
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-17 09:20:48 -0700

    Comment changes

--- Module1/TO1-2/main.cpp
+++ Module1/TO1-2/main.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 int main()
 {
-    FactorMod3(); //Function call found in factor.h referenced above in #include "factor.h" 
-                //  Defined in factor.cpp
-
+    FactorMod3(); //Function call found in factor.h referenced above in #include "factor.h" Defined in factor.cpp
     Test();
     FactorMod3();
     return 0;
