0d5f4cb7785c69bcbd6498495a2c4b648bccecd8
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-17 11:48:46 -0700

    initial commit

--- Module1/TO1-4/src/main.cpp
+++ Module1/TO1-4/src/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 // Main function
 int main() 
 {
-	int age = 13;
+	int age = 95;
 	cout << "Current age: " << age << endl; // Prints 95
-	// ByValueAgeIn5Years(age);
+	ByValueAgeIn5Years(age);
   	ByReferenceAgeIn5Years(age);
   	cout << "In 5 years, your age is: " << CurrentAge(age) << endl;
   	if (age >= 18)
