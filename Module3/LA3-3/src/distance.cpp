13af5c73cce290b27bc3936ed8c01aacb130bc0c
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 08:42:13 -0700

    test

--- Module3/LA3-3/src/distance.cpp
+++ Module3/LA3-3/src/distance.cpp
@@ -5,6 +5,20 @@
 // using namespace std;
 #include "distance.h"
 
+/**
+ * @brief Plus + Operator for Distance objects
+ * @param d2 Second Distance object
+ */
+Distance Distance::operator + (Distance rhs) const
+{
+    Distance temp;
+    int feet = feet_ + rhs.feet_;
+    float inches = inches_ + rhs.inches;
+    Distance temp(feet, inches);
+    
+    return temp;
+}
+
 void Distance::ShowDist() const
 {
     std::cout << feet() << "\'-" << inches() << "\"" << std::endl; 
